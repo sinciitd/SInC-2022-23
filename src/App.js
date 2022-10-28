947054b9034cdf968514de9d6e7dc3b1fbf64e73
Jaskaran Singh Bhalla <68080441+ultimatejaskaran@users.noreply.github.com>
2022-10-28 11:26:39 +0530

    Commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Header from './components/header/Header'
+import Hero from './components/hero/Hero'
+import Events from './components/events/Events'
+import AboutUs from './components/about-us/AboutUs'
+import Contact from './components/contact/Contact'
+import Register from './components/register/Register'
+import Footer from './components/footer/Footer'
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Hero/>
+      <Events/>
+      <AboutUs/>
+      <Contact/>
+      <Register/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
